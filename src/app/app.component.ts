c0eeb4afb08b632ad1fb521b43750ed6b2bb086a
waelmk <33528076+waelmk@users.noreply.github.com>
2018-12-31 16:19:51 +0100

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {promise} from 'selenium-webdriver';
+import {AppareilService} from './services/appareil.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isAuth = false;
-
-  appareils = [
-    {
-      name: 'Machine à laver',
-      status: 'éteint'
-    },
-    {
-      name: 'Frigo',
-      status: 'allumé'
-    },
-    {
-      name: 'Ordinateur',
-      status: 'éteint'
+  lastUpdate = new Promise(
+    (resolve, reject) => {
+     const date =new Date();
+     setTimeout(
+       () => {
+         resolve(date);
+       }, 2000
+     )
     }
-  ];
+  );
 
-  constructor() {}
+appareils: any[];
 
+  constructor(private appareilService: AppareilService) {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+  onAllumer() {
+    this.appareilService.switchOnAll();
+  }
+  onEteindre() {
+    this.appareilService.switchOffAll();
+  }
+
+  ngOnInit() {
+    this.appareils = this.appareilService.appareils;
+  }
 }
