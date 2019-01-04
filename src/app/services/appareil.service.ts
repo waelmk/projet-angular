4a1596346a35df4956b5ec6f53b36b34f003d9d8
waelmk <33528076+waelmk@users.noreply.github.com>
2019-01-04 16:54:49 +0100

    Add files via upload

--- src/app/services/appareil.service.ts
+++ src/app/services/appareil.service.ts
@@ -1,35 +1,50 @@
 import {forEach} from '@angular/router/src/utils/collection';
 
 export class AppareilService {
-  appareils = [
+  private appareils = [
     {
+      id: 1,
       name: 'Machine à laver',
       status: 'éteint'
     },
     {
+      id: 2,
       name: 'Télévision',
       status: 'allumé'
     },
     {
+      id: 3,
       name: 'Ordinateur',
       status: 'éteint'
     }
   ];
 
-  switchOnAll(){
-    for(let appareil of this.appareils){
+  getAppareilById(id: number) {
+    const appareil = this.appareils.find(
+      (appareilObject) => {
+        return appareilObject.id === id;
+      }
+    );
+    return appareil;
+  }
+
+  switchOnAll() {
+    for (let appareil of this.appareils) {
       appareil.status = 'allumé';
     }
   }
-  switchOffAll(){
-    for(let appareil of this.appareils){
+
+  switchOffAll() {
+    for (let appareil of this.appareils) {
       appareil.status = 'éteint';
     }
   }
-  switchOnOne(index: number){
+
+  switchOnOne(index: number) {
     this.appareils[index].status = 'allumé';
   }
-  switchOffOne(index: number){
+
+  switchOffOne(index: number) {
     this.appareils[index].status = 'éteint';
   }
 
