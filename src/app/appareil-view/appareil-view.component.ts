94c676e5e1b5f16a8f151e5c96694a7f2dfdc5f6
waelmk <33528076+waelmk@users.noreply.github.com>
2019-01-04 16:55:56 +0100

    Add files via upload

---
+++ src/app/appareil-view/appareil-view.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import {AppareilService} from '../services/appareil.service';
+
+@Component({
+  selector: 'app-appareil-view',
+  templateUrl: './appareil-view.component.html',
+  styleUrls: ['./appareil-view.component.css']
+})
+export class AppareilViewComponent implements OnInit {
+
+  isAuth = false;
+  lastUpdate = new Promise(
+    (resolve, reject) => {
+      const date =new Date();
+      setTimeout(
+        () => {
+          resolve(date);
+        }, 2000
+      )
+    }
+  );
+
+  appareils: any[];
+
+  constructor(private appareilService: AppareilService) {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+  onAllumer() {
+    this.appareilService.switchOnAll();
+  }
+  onEteindre() {
+    this.appareilService.switchOffAll();
+  }
+
+  ngOnInit() {
+    this.appareils = this.appareilService.appareils;
+  }
+
+}
